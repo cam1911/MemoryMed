4a7200ca5069674cc1579e0f4aaaab9e8d486155
Cameron <cameronmanley@sbcglobal.net>
2015-02-05 19:15:29 -0600

    Updated main screen. Added Medfriend class

---
+++ SE4351_class_project-master/MedMemory/src/com/example/medmemory/model/Medfriend.java
@@ -0,0 +1,32 @@
+package com.example.medmemory.model;
+
+/**
+ * Created by Cameron on 2/5/2015.
+ * This class is the model for a Medfriend. A Medfriend will be the recipient of an SMS or Push message stating the main user has not taken their medication on time.
+ */
+public class Medfriend {
+
+    String name;
+    String number;
+
+    public Medfriend(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
